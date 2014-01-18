b64c0f3fa0194b07bebf68c82ce9ef250d46925c
garwedgess <garethwilliams21@gmail.com>
2014-01-18 21:20:34 +0530

    [1/5] Breathing missedcall/sms/voicemail
    Adds a breathing effect to missed call/SMS/voicemail notifications.
    
    Change-Id: I62b41064956e8ddd409d66ecf54004b2469c2ad8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2970,6 +2970,24 @@ public final class Settings {
         public static final String ENABLE_MWI_NOTIFICATION = "enable_mwi_notification";
 
         /**
+         * Give SMS notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_SMS_BREATH = "sms_breath";
+
+        /**
+         * Give Missed call notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_MISSED_CALL_BREATH = "missed_call_breath";
+
+        /**
+         * Give Voicemail notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_VOICEMAIL_BREATH = "voicemail_breath";
+
+        /**
          * Show pointer location on screen?
          * 0 = no
          * 1 = yes
