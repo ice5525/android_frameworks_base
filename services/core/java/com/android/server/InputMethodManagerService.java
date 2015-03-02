c1356c9b063ade13da683688b518a660a64586d9
Adnan Begovic <adnan@cyngn.com>
2015-03-02 10:05:45 +0000

    Base: don't show headsup if IME is showing
    taken from this commit https://github.com/Mahdi-Rom/android_frameworks_base/commit/18294da23b44fa2c24f3759e970f1ee5ac9fedc9
    
    Change-Id: I9edb1b74c0ee5c38d5e82f0ade0ecd7221dfa5a2

--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -2237,6 +2237,11 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
     }
 
     @Override
+    public boolean isImeShowing() {
+        return mInputShown;
+    }
+
+    @Override
     public void showInputMethodPickerFromClient(IInputMethodClient client) {
         if (!calledFromValidUser()) {
             return;
