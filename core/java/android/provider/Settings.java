7fd0399a5c5fd28781269bbfbc2b92b9060a342c
Jmz <jmztaylor@gmail.com>
2015-03-02 20:07:07 +0100

    Force Expanded Notifications (1/2)
    This will force expanded notifications for all apps that support
    expanded notifications
    
    Change-Id: I1f6fac658a3e58488bc37cafec9208a75da78ab8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3832,6 +3832,12 @@ public final class Settings {
         public static final String VOLUME_KEYS_DEFAULT = "volume_keys_default";
 
         /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
