SimpleAOSP-Lollipop/platform_frameworks_base

QS: Add SecondaryClick for all tiles
Now that we got QS customization, users can select any tile they want in first row (enlarged).
Enable secondaryclick for all other tiles

Signed-off-by: snak3ater <saeedyasir91@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/RotationLockTile.java
@@ -58,6 +58,13 @@ public class RotationLockTile extends QSTile<QSTile.BooleanState> {
     }
 
     @Override
+    protected void handleSecondaryClick() {
+        intent.setClassName("com.android.settings",
+				"com.android.settings.Settings$DisplayRotationSettingsActivity");
+        mHost.startSettingsActivity(intent);
+    }
+
+    @Override
     protected void handleLongClick() {
 	intent.setClassName("com.android.settings",
 				"com.android.settings.Settings$DisplayRotationSettingsActivity");

ice5525/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/RotationLockTile.java
@@ -79,6 +79,11 @@ public class RotationLockTile extends QSTile<QSTile.BooleanState> {
     }
 
     @Override
+    protected void handleSecondaryClick() {
+        mHost.startSettingsActivity(DISPLAY_SETTINGS);
+    }
+
+    @Override
     protected void handleLongClick() {
         if (!mAdvancedMode) {
             mHost.startSettingsActivity(DISPLAY_SETTINGS);