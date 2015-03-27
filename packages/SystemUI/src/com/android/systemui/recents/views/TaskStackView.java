731c0b6e7a4100a74f1f68179521167a6a89c380
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-27 09:32:53 +0000

    SystemUI: recent panel fixes
    add:
    https://github.com/Euphoria-OS/android_frameworks_base/commit/34d6f23731cddc8fd903f25cb82d1c0a56f835c5
    
    revert:
    systemui: animate clear recents button on exit animation
    https://github.com/CyanogenMod/android_frameworks_base/commit/16551c5307d54f2a5993f02c4af02d538fabf37d
    
    systemui: adjust position of clear recents button
    https://github.com/CyanogenMod/android_frameworks_base/commit/4725e899158828b3e28e7148759e75713bc2f581
    
    systemui: never show excluded recent task in recents view
    https://github.com/CyanogenMod/android_frameworks_base/commit/5507474632f1135e6ee90908b57c595aae9b4636
    
    Change-Id: I3eb1ea0365ef338f4af8fd342dab099f9f5b00a8

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -28,7 +28,6 @@ import android.content.pm.PackageManager;
 import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.net.Uri;
-import android.os.UserHandle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -542,18 +541,6 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
         tv.dismissTask(0L);
     }
 
-    /** Resets the focused task. */
-    void resetFocusedTask() {
-        if ((0 <= mFocusedTaskIndex) && (mFocusedTaskIndex < mStack.getTaskCount())) {
-            Task t = mStack.getTasks().get(mFocusedTaskIndex);
-            TaskView tv = getChildViewForTask(t);
-            if (tv != null) {
-                tv.unsetFocusedTask();
-            }
-        }
-        mFocusedTaskIndex = -1;
-    }
-
     private boolean dismissAll() {
         return Settings.System.getInt(mContext.getContentResolver(),
             Settings.System.RECENTS_CLEAR_ALL_DISMISS_ALL, 1) == 1;
@@ -594,12 +581,20 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                         }
                     }
                 }
-
-                // And remove all the excluded or all the other tasks
-                SystemServicesProxy ssp = RecentsTaskLoader.getInstance().getSystemServicesProxy();
-                ssp.removeAllUserTask(UserHandle.myUserId());
             }
         });
+    }
+
+    /** Resets the focused task. */
+    void resetFocusedTask() {
+        if ((0 <= mFocusedTaskIndex) && (mFocusedTaskIndex < mStack.getTaskCount())) {
+            Task t = mStack.getTasks().get(mFocusedTaskIndex);
+            TaskView tv = getChildViewForTask(t);
+            if (tv != null) {
+                tv.unsetFocusedTask();
+            }
+        }
+        mFocusedTaskIndex = -1;
     }
 
     @Override
