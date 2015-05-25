aa6ad8b7b219dc724f34c0fe2ce02553a942c87f
haiyangl <haiyangl@codeaurora.org>
2015-05-25 14:57:30 +0200

    SystemUI: Add task manager
    Add a button in status bar expanded header to switch to task manager,
    which supports to watch current running tasks and usage of memory, also
    supports to close one or all of running tasks.
    
    Change-Id: Ic06df8171e741c2ead63e077421c172624094ab0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -130,6 +130,7 @@ import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.PathInterpolator;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -349,6 +350,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     View mFlipSettingsView;
     private QSPanel mQSPanel;
     private DevForceNavbarObserver mDevForceNavbarObserver;
+
+    // task manager
+    private TaskManager mTaskManager;
+    private LinearLayout mTaskManagerPanel;
+    private ImageButton mTaskManagerButton;
+    private boolean showTaskList = false;
 
     // top bar
     StatusBarHeaderView mHeader;
@@ -1282,6 +1289,22 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             });
         }
 
+        // task manager
+        if (mContext.getResources().getBoolean(R.bool.config_showTaskManagerSwitcher)) {
+            mTaskManagerPanel =
+                    (LinearLayout) mStatusBarWindow.findViewById(R.id.task_manager_panel);
+            mTaskManager = new TaskManager(mContext, mTaskManagerPanel);
+            mTaskManager.setActivityStarter(this);
+            mTaskManagerButton = (ImageButton) mHeader.findViewById(R.id.task_manager_button);
+            mTaskManagerButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    showTaskList = !showTaskList;
+                    mNotificationPanel.setTaskManagerVisibility(showTaskList);
+                }
+            });
+        }
+
         // Set up the initial custom tile listener state.
         try {
             mCustomTileListenerService.registerAsSystemService(mContext,
@@ -1292,7 +1315,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         mQSPanel.getHost().setCustomTileListenerService(mCustomTileListenerService);
-
         // User info. Trigger first load.
         mHeader.setUserInfoController(mUserInfoController);
         mKeyguardStatusBar.setUserInfoController(mUserInfoController);
@@ -2808,6 +2830,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mWaitingForKeyguardExit = false;
         disable(mDisabledUnmodified, !force /* animate */);
         setInteracting(StatusBarManager.WINDOW_STATUS_BAR, true);
+        if (mContext.getResources().getBoolean(R.bool.config_showTaskManagerSwitcher)) {
+            mTaskManager.refreshTaskManagerView();
+        }
     }
 
     public void animateCollapsePanels() {
